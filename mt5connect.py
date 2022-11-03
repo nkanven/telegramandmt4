a5118be8c3f96f92cf1417b05ecf48eade34bafe
Nkondog A. Venceslas <nkanven@gmail.com>
2022-11-03 11:50:05 +0100

    start python to mt5 connector

---
+++ mt5connect.py
@@ -0,0 +1,24 @@
+import MetaTrader5 as mt5
+# display data on the MetaTrader 5 package
+print("MetaTrader5 package author: ",mt5.__author__)
+print("MetaTrader5 package version: ",mt5.__version__)
+ 
+# establish MetaTrader 5 connection to a specified trading account
+mt5.initialize()
+
+login=48133064
+server="HFMarketsGlobal-Demo"
+password="gwLsYRx10"
+
+
+authorized=mt5.login(login, password, server)
+if authorized:
+    # display trading account data 'as is'
+    print(mt5.account_info())
+    # display trading account data in the form of a list
+    print("Show account_info()._asdict():")
+    account_info_dict = mt5.account_info()._asdict()
+    for prop in account_info_dict:
+        print("  {}={}".format(prop, account_info_dict[prop]))
+else:
+    print("failed to connect at account #{}, error code: {}".format(login, mt5.last_error()))
